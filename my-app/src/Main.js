d4c729402a0b15467619e9d09940fcf69f1fb572
Joey <joseph.r.young2002@gmail.com>
2021-09-02 16:29:06 -0500

    filtering

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -2,23 +2,50 @@ import React from 'react'
 import HornedBeast from './components/Horned-Beasts'
 import data from './data/data.json'
 import Form from 'react-bootstrap'
-
+import Container from 'react-bootstrap'
 
 
 
 class Main extends React.Component {
+
+constructor(props){
+
+}
+
+
+handleSubmit = (event) => {
+  event.preventDefault();
+
+  let hornCount = parseInt(event.target.elements.hornCount.value);
+  console.log(hornCount);
+  if (hornCount > 0){
+    this.setState({
+      filteredBeasts: this.props.beasts.filter(beast => {
+        if (beast.horns === hornCount) {
+          return true;
+        }
+        return false;
+      })
+    })
+    }
+  }
+}
+
   render() {
+let beasts = this.state.filteredBeasts;
+console.log(beasts);
+
   return (
 
 <Container as="main">
-    <Form>
+    <Form onSubmit={this.handleSubmit}>
       <Form.Group>
         <Form.Control as="select"> 
-          <option value="1"></option>
-          <option value="2"></option>
-          <option value="3"></option>
-          <option value="4"></option>
-          <option value="5"></option>
+          <option value="0">All</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          
 
         </Form.Control>
 
