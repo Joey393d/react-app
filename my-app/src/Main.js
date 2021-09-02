1ead23268faab09dd4616fc2636ed5da11ba53b0
Joey <joseph.r.young2002@gmail.com>
2021-09-02 14:08:18 -0500

    add a submit via bootstrap

--- my-app/src/Main.js
+++ my-app/src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import HornedBeast from './components/Horned-Beasts'
 import data from './data/data.json'
+import Form from 'react-bootstrap'
 
 
 
@@ -8,10 +9,29 @@ import data from './data/data.json'
 class Main extends React.Component {
   render() {
   return (
+
+<Container as="main">
+    <Form>
+      <Form.Group>
+        <Form.Control as="select"> 
+          <option value="1"></option>
+          <option value="2"></option>
+          <option value="3"></option>
+          <option value="4"></option>
+          <option value="5"></option>
+
+        </Form.Control>
+
+      </Form.Group>
+    </Form>
+
+
+
     <div>
     {data.map(beast => (<HornedBeast title={beast.title} image= {beast.image_url} 
       description={beast.description}/>))}
     </div>
+    </Container>
   );
   };
 };
